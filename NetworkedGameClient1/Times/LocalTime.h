d76ed8a2b88c620dbce66c21454e04dd074eb2b0
HNoodles <13671980486@163.com>
2019-11-13 01:28:20 -0500

    modified implementation of timelines

--- NetworkedGameClient1/Times/LocalTime.h
+++ NetworkedGameClient1/Times/LocalTime.h
@@ -5,13 +5,10 @@ class LocalTime : public Timeline
 {
 private:
 	Timeline& anchor;
-	double step_size;
 public:
 	LocalTime(double step_size, Timeline& anchor);
 
 	void setPaused(bool p) override;
-
-	void setStepSize(double step_size) override;
 
 	double getTime() override;
 };
