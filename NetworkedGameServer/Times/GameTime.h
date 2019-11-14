f6b00b5c4719fbcd6fc8d472c7ec75e411be759b
HNoodles <13671980486@163.com>
2019-11-14 13:36:13 -0500

    updated time module for server

--- NetworkedGameServer/Times/GameTime.h
+++ NetworkedGameServer/Times/GameTime.h
@@ -1,19 +1,18 @@
 #pragma once
 #include "Timeline.h"
 
-//using namespace std::chrono;
-
 class GameTime : public Timeline
 {
 private:
 	std::chrono::nanoseconds start_time;
-	double step_size;
 public:
 	GameTime(double step_size);
 
-	void setPaused(bool p) override;
+	//GameTime(double step_size, double time);
 
-	void setStepSize(double step_size) override;
+	void resetStepSize(double new_step_size);
+
+	void setPaused(bool p) override;
 
 	double getTime() override;
 };
--- NetworkedGameServer/Times/Timeline.h
+++ NetworkedGameServer/Times/Timeline.h
@@ -6,10 +6,16 @@ using namespace std::chrono;
 class Timeline
 {
 protected:
+	double step_size;
 	bool paused = false;
 	double pausedTics = 0;
 	double lastPausedPoint = 0;
 public:
+	Timeline(double step_size)
+	{
+		setStepSize(step_size);
+	}
+
 	std::chrono::nanoseconds getNowRealTime()
 	{
 		return duration_cast<std::chrono::nanoseconds>(system_clock::now().time_since_epoch());
@@ -20,9 +26,17 @@ public:
 		return paused;
 	}
 
-	virtual void setPaused(bool p) = 0;
+	void setStepSize(double step_size)
+	{
+		this->step_size = step_size * 1e9;
+	}
 
-	virtual void setStepSize(double step_size) = 0;
+	double getStepSize()
+	{
+		return step_size / 1e9;
+	}
+
+	virtual void setPaused(bool p) = 0;
 
 	virtual double getTime() = 0;
 };
